4e13f32ee0fa23c2bdf9658a4a9f8955d955e2b6
HHH-603 <hholmes726@gmail.com>
2021-04-10 16:34:38 -0400

    Added Ben Kenobi gif to my 404 page.

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,6 +5,10 @@ const PageNotFound = () => {
   return (
     <Layout>
       <h1>Page Not Found</h1>
+      <img
+        src="https://media.giphy.com/media/4560Nv2656Gv0Lvp9F/giphy.gif"
+        alt="these-are-not-the-droids-you-are-looking-for"
+      />
       <p>This is not the page you are looking for.</p>
     </Layout>
   );
