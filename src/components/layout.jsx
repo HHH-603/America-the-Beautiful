3b1723be333a5248843f0da51af8553d191c703a
HHH-603 <hholmes726@gmail.com>
2021-04-27 15:46:19 -0400

    Added new font family for link_navbar-header and h1 and h2.

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../styles/main.scss";
 import Footer from "./footer";
 import Navigation from "./navigation";
+import "@fontsource/lobster";
 
 const Layout = (props) => {
   return (
